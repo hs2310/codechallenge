c4d441625bd05cf9175186ae846635baf3bfcaf7
hs2310 <harshilshah186@gmail.com>
2020-07-17 04:51:40 -0700

    Java Compile Code

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,53 @@
 var express = require('express');
-
+var bodyParser = require("body-parser");
+var cors = require('cors');
+var {java_compilation_path, java_execution_path } = require('./pathvariable');
+const {c, cpp, node, python, java} = require('compile-run');
 var app = express();
+var fs = require('fs');
+
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+});
 
 app.get('/' , (req,res) => {
     res.send('Hello World');
 })
 
+app.post('/compile' , (req,res) => {
+    console.log(req.body);
+    if(req.body.lang == "java"){
+        fs.writeFile('Solution.java', req.body.code, function (err) {
+            if (err)  console.log(err);
+            else {
+                console.log("File Created");
+                java.runFile('./Solution.java',{ stdin:'3\n2\n'},{
+                    compilationPath: java_compilation_path,
+                    executionPath: java_execution_path
+                }).then(
+                    result=>{
+                        console.log(result)
+                        res.send(result)
+                    }
+                ).catch(err =>{ 
+                    console.log(err)
+                    res.send(err)
+                });
+                
+            }
+          });
+    }
+    
+})
 app.listen(3001);
 console.log("Server running on port 3001");
--- frontend/src/components/editor/Editor.js
+++ frontend/src/components/editor/Editor.js
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AceEditor from "react-ace";
- 
+import axios from 'axios'; 
+
 import "ace-builds/src-noconflict/mode-java";
 import "ace-builds/src-noconflict/theme-xcode";
  
-function onChange(newValue) {
-  console.log("change", newValue);
-}
-
 function Editor() {
-    return (
+  const [code , setCode] = useState("")
+  const [output , setOutput] = useState("");
+  return (
+      <div>
         <div>
-            <AceEditor
-              mode="java"
-              theme="xcode"
-              onChange={onChange}
-              name="UNIQUE_ID_OF_DIV"
-              editorProps={{ $blockScrolling: true }}
-            />
+          <AceEditor
+            mode="java"
+            theme="xcode"
+            onChange={(e) => {setCode(e)}}
+            name="UNIQUE_ID_OF_DIV"
+            editorProps={{ $blockScrolling: true }}
+          />
+      <button type="button" onClick = {async () => {
+        console.log(code);
+        let data = {
+          code : code,
+          lang : "java",
+        }
+        await axios.post("http://localhost:3001/compile", data)
+        .then(res => setOutput(res.data))
+        .catch( err => console.log(err))
+        }}>SUBMIT</button>
         </div>
-          )
+        <div style={{padding: "2%"}}>
+          <pre>
+            {output.exitCode ? output.stderr : output.stdout}
+          </pre>
+        </div>
+      </div>
+        )
 }
 
 export default Editor;
